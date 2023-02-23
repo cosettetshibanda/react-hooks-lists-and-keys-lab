58ecc33a18e6c48c4922952f9e9f65e6b70b0a38
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-02-23 00:51:07 -0700

    commit tree a609c439b137546896a23f59a5aa3bc5079f9510

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const map = links.map((link) => (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+  ));
+  return <nav>{map}</nav>;
 }
 
 export default NavBar;
